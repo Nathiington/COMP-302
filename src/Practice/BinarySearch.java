5f0aa4e0e1035e912108e6b7bb6d7e0cb3698248
Nathan <58734424+Nathiington@users.noreply.github.com>
2021-05-01 00:53:35 +0200

    Searching practice

---
+++ src/Practice/BinarySearch.java
@@ -0,0 +1,67 @@
+package Practice;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+//Binary Search takes an array, splits it into 2, then checks to see if the desired value is less than, greater than or equal to our desired number
+//If it is greater than, the end of our array now becomes the midpoint and this process is repeated until the desired value is found
+//If it is less than, the beginning of the array is shifted to the midpoint and the process is repeated
+
+//the worst case is Math.log(2.0) n where n is the size of the array, in this case 4, meaning the code will take at most 4 cycles to find the desired value
+//the best case is 1
+
+public class BinarySearch
+{
+    static int search(int[] arr, int x)
+    {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (right >= left)
+        {
+//        To calculate the midpoint of our array
+            int mid = left + (right-left)/2;
+
+//         To check if the midpoint of our array is our desired value
+            if (arr[mid] == x)
+            {
+                return mid;
+            }
+
+//          To check if the value we want is in the left half of our array
+            if (arr[mid] > x)
+            {
+                right = mid -1;
+            }
+//          Else our desired value is in the right half of the array
+            else
+            {
+                left = mid +1;
+            }
+//Repeat loop until our desired value is our mid
+        }
+//Then in our function we have a while loop to go through the array and return our desired value's index else return an arbitrary int
+        return  -0;
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner scanf = new Scanner(System.in);
+        int myArr[] = { 2, 3, 4, 10, 40,50,60,80,100,110,200,300,550,670,800,900};
+        System.out.println(Arrays.toString(myArr));
+        System.out.println("Select a number to locate the index");
+        System.out.println(myArr.length);
+        int x = scanf.nextInt();
+
+        int result = search(myArr,x);
+        if (result == -0)
+        {
+            System.out.println("Element is not in the array");
+        }
+        else
+        {
+            System.out.println("Element is at index: " + result);
+        }
+
+    }
+}
---
+++ src/Practice/LinearSearch.java
@@ -0,0 +1,48 @@
+package Practice;
+
+import java.util.*;
+
+//Searches ka Setswana
+//Goes through the array to locate the item you input for it to search
+//The worst case is n where n is the size of the array
+//The best case is 1
+
+public class LinearSearch
+{
+//Search function where we pass an array and the number we want in the array as arguments
+//Then in our function we have a for loop to go through the array and return our desired value's index else return an arbitrary int
+    public static int search(int[] arr, int x)
+    {
+        int n = arr.length;
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] == x)
+            {
+                return  i;
+            }
+        }
+        return -0;
+    }
+
+
+//tester
+    public static void main(String[] args)
+    {
+        Scanner scanf = new Scanner(System.in);
+
+        int myArr[] = {5,12,23,2,3,4,34,45,67};
+        System.out.println(Arrays.toString(myArr));
+        System.out.println("Select a number to locate the index");
+        int x = scanf.nextInt();
+
+        int result = search(myArr,x);
+        if (result == -0)
+        {
+            System.out.println("Element is not in the array");
+        }
+        else
+        {
+            System.out.println("Element is at index: " + result);
+        }
+    }
+}
